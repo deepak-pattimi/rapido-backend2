9041aae22c6ef609a6b3f515c4ba153f563c5c06
Deepak <pattimideepak777@gmail.com>
2026-09-09 09:25:26 +0530

    v0.0.3

--- src/models/Customer.js
+++ src/models/Customer.js
@@ -10,6 +10,7 @@ const customerSchema = new mongoose.Schema({
     email: {
         type: String,
         required: false,
+        unique: true,
         sparse: true,
         lowercase: true,
         trim: true
